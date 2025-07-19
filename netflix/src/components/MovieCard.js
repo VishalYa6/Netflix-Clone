adfffb54f6f0423cecde99603517f6d725ff2c5c
Vishal Yadav <vy372984@gmail.com>
2025-07-19 15:01:49 +0530

    now complete

--- netflix/src/components/MovieCard.js
+++ netflix/src/components/MovieCard.js
@@ -3,6 +3,7 @@ import { Banner_URL } from '../utils/constant'
 
 
 const MovieCard = ({posterPath}) => {
+  if(posterPath === null ) return null ;
   return (
     <div className='w-48 pr-2'>
         <img src={`${Banner_URL}/${posterPath}`} alt='movie-banner'/>
