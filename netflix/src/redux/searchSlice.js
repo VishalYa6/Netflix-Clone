b726a493079bf1b568917ce35be1a7cdd9c1a6ca
Vishal Yadav <vy372984@gmail.com>
2025-07-19 14:59:50 +0530

    search Movie complete

--- netflix/src/redux/searchSlice.js
+++ netflix/src/redux/searchSlice.js
@@ -1,1 +1,24 @@
-import 
+import {createSlice} from "@reduxjs/toolkit";
+
+const searchSlice = createSlice({
+    name : "search",
+    initialState:{
+        movieName : null,
+        searchedMovie : null
+    },
+    reducers:{
+        //actions 
+
+        setSearchMovieDetails:(state,action)=>{
+
+            const {searchMovie, movies} = action.payload;
+            state.movieName = searchMovie;
+            state.searchedMovie = movies;
+            
+        }
+
+    }
+})
+
+export const {setSearchMovieDetails} = searchSlice.actions;
+export default searchSlice.reducer;
